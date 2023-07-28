a666fe892fbad8d163aa1d35b092cb4efe6ca30c
Isaac Torbett <110787538+VirtualMarten@users.noreply.github.com>
2023-07-28 19:45:50 -0400

    readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='xsnotif',
     version='0.0.1',
-    description='',
+    description='XS Overlay notifications API',
     long_description=open('README.md').read(),
     author='Isaac Torbett',
     author_email='izacht13@email.com',
